0785966219ecccda3e6e3fc011d71dfab0439336
Tony Leung <tony.leung@cruzium.com>
2025-12-27 00:33:30 +0000

    Add provider for configuring the plugin globally

--- angular-stylishselect.js
+++ angular-stylishselect.js
@@ -1,26 +1,49 @@
 /**
  * AngularJS wrapper for Stylish Select
- * @version 1.0.0
+ * @version 1.1.0
  * @author Tony Leung <tony.leung@cruzium.com>
  * @copyright Copyright (c) 2025 Cruzium Digital
  * @license https://opensource.org/license/gpl-3-0/ GPL-3.0-only
  */
 
 'use strict';
-angular.module('ngStylishselect', []).directive('ngStylishselect', function($timeout, $parse) {
-	return({
-		require: 'ngModel',
-		link: function($scope, $element, $attrs, ngModel) {
-			return($timeout(function() {
-				var instance = new StylishSelect($element[0]);
-				$scope.$watch($attrs['ngModel'], function(value) {
-					instance.refresh();
-				});
-				$element.on('change', function(e) {
-					ngModel.$setViewValue($attrs['value'] !== undefined ? $attrs['value'] : $element.val());
-					$scope.$apply();
-				});
-			}));
-		}
+(function() {
+	var module = angular.module('ngStylishselect', []);
+
+	module.provider('ngStylishselectConfig', function () {
+		var defaults = {};
+		this.setDefaults = function(options) {
+			angular.extend(defaults, options);
+		};
+		this.$get = function() {
+			return {
+				get: function() {
+					return(angular.copy(defaults));
+				}
+			};
+		};
 	});
-});
+
+	module.directive('ngStylishselect', function($parse, $timeout, ngStylishselectConfig) {
+		return({
+			require: 'ngModel',
+			restrict: 'A',
+			link: function($scope, $element, $attrs, ngModel) {
+				return($timeout(function() {
+					var opts = ngStylishselectConfig.get();
+					if ($attrs.ngStylishselect) {
+						angular.extend(opts, $parse($attrs.ngStylishselect)());
+					}
+					var instance = new StylishSelect($element[0], opts);
+					$scope.$watch($attrs['ngModel'], function(newValue, oldValue) {
+						instance.refresh();
+					});
+					$element.on('change', function(e) {
+						ngModel.$setViewValue($attrs['value'] !== undefined ? $attrs['value'] : $element.val());
+						$scope.$apply();
+					});
+				}));
+			}
+		});
+	});
+})();
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Alpine.js Wrapper for Stylish Select
+
+## Dependencies
+
+- [Stylish Select](https://cdn.cruzium.info/plugin/stylish-select/)
+
+## Installation
+
+*Include JS file*
+
+```html
+<script type="text/javascript" src="https://cdn.cruzium.info/angular-stylishselect/latest/angular-stylishselect.min.js"></script>
+```
+
+## Usage
+
+*Basic HTML Markup*
+```html
+<select name="foo" id="foo" ng-model="foo" ng-stylishselect>
+	<option value="">Please select</option>
+	<option value="1">Option 1</option>
+	<option value="2">Option 2</option>
+	<option value="3">Option 3</option>
+</select>
+```
+
+Note: The plugin requires attribute `ng-model` to work.
+
+*With Per-instance Options*
+```html
+<select name="foo" id="foo" ng-model="foo" ng-stylishselect="{ ... }">
+	<option value="">Please select</option>
+	<option value="1">Option 1</option>
+	<option value="2">Option 2</option>
+	<option value="3">Option 3</option>
+</select>
+```
+
+*To Set Default Options Globally*
+```javascript
+app.config(function(ngStylishselectConfigProvider) {
+	ngStylishselectConfigProvider.setDefaults({
+		...
+	});
+});
+```
+
+## Options
+
+Refer to [Options](https://cdn.cruzium.info/doc/stylish-select/latest/readme.md#options) section.
+
+## Limitations
+
+Refer to [Limitiations](https://cdn.cruzium.info/doc/stylish-select/latest/readme.md#limitations) section.
